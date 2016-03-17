99d7f8a8f86a888abaf059506c8c331b23a679b6
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-17 14:37:41 -0400

    routing and partials are up and running

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+var tabbyApp = angular.module('tabbyApp', ['ngRoute']);
+
+tabbyApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', { //main page
+                templateUrl : 'landing-page.html',
+            })
+            .when('/NewCat',  {
+                templateUrl : 'new-cat.html',
+            })
+            .when('/DetailsEdit', {
+                templateUrl : 'details-edit.html',
+            })
+            .when('/Cart', {
+                templateUrl : 'cart.html',
+            })
+          });
---
+++ assets/js/controllers.js
@@ -0,0 +1,5 @@
+tabbyApp.controller('mainController', function($scope) {
+    console.log("this works?")
+    // create a message to display in our view
+    $scope.message = 'THIS WORKS, I HOPE';
+  });
