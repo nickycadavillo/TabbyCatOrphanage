132d96e6005ea0aa9a46989fe583dbbcfc2ec6a1
BestMatEver <mattkun@gmail.com>
2016-03-18 14:02:39 -0400

    we got posting to the json file to work

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -8,9 +8,20 @@ tabbyApp.controller('mainController', function($scope) {
 //landing page controller to get all the cats from json file
 tabbyApp.controller('landingPageController', [ '$http', '$scope', function($http, $scope){
   $scope.message = "SCOPE IS WORKING";
-  $http.get('assets/json/tabbycat.json').success(function(data){
-    $scope.cats = data
+
+  // $.ajax({
+  //   type: 'GET',
+  //   url: 'localhost:3001/allCats',
+  //   data: allCats,
+  //   type: "json",
+  // }).done(function(allCats){$scope.cats = allCats;});
+
+  //this seems to work!
+  $http.get('http://localhost:3000/allCats').success(function(data){
+    $scope.cats = data;
+    console.log($scope.cats);
   });//close get
+
 }]);//close controller
 
 //https://stormy-meadow-75496.herokuapp.com/cat/index
@@ -18,7 +29,7 @@ tabbyApp.controller('landingPageController', [ '$http', '$scope', function($http
 // new cat controller to update json file
 tabbyApp.controller('newCatController', [ '$http', '$scope', function($http, $scope){
     $scope.something = "SCOPE IS WORKING";
-    var newCat = "this is a new cat!";
+
     function postSuccess(){
       console.log("success!");
     };
@@ -27,8 +38,23 @@ tabbyApp.controller('newCatController', [ '$http', '$scope', function($http, $sc
     };
 
     $('.submit-btn').click(function(){
+      var newCat = {'name': 'JERKFACE','type': 'Special','price': 10,'desc': 'This is a gray cutie with an adorable baseball hat.','image': 'assets/images/fluffy-knight.png','reviews': [{'revName': 'Nicky C','text': 'This is the best cat ever. Yay!','stars': 5}]};
+
+
       console.log("we clicked");
-      $http.post('assets/json/tabbycat.json', newCat).then(postSuccess, postFailure);
+
+      $.ajax({
+        method: "POST",
+        url: 'http://localhost:3000/allCats',
+        datatype: 'json',
+        data: newCat,
+      }).done(function()
+      {
+        console.log("posted!");
+      });
+
+        //this angular.js implimentation of getting and posting to json. not working they might work...?
+      // $http.post('http://localhost:3000/allCats', newCat).then(postSuccess, postFailure);
     })
 
 
